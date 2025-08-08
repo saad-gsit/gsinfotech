62c2429b7a9243d244a0cc037a651f82afaf6023
saad-gsit <saad.gsit@gmail.com>
2025-08-08 16:29:56 +0530

    feat: Update development database configuration with specific credentials and host

--- server/config/database.js
+++ server/config/database.js
@@ -2,10 +2,15 @@ require('dotenv').config();
 
 module.exports = {
     development: {
-        username: process.env.DB_USER || 'root',
-        password: process.env.DB_PASSWORD || 'root',
-        database: process.env.DB_NAME || 'gsinfotech_db',
-        host: process.env.DB_HOST || 'localhost',
+        // username: process.env.DB_USER || 'root',
+        // password: process.env.DB_PASSWORD || 'root',
+        // database: process.env.DB_NAME || 'gsinfotech_db',
+        // host: process.env.DB_HOST || 'localhost',
+        // port: process.env.DB_PORT || 3306,
+        username: process.env.DB_USER || 'u226800403_gsinfo',
+        password: process.env.DB_PASSWORD || 'Gsinfo@3686',
+        database: process.env.DB_NAME || 'u226800403_gsinfotech_db',
+        host: process.env.DB_HOST || 'srv1474.hstgr.io',
         port: process.env.DB_PORT || 3306,
         dialect: 'mysql',
         logging: console.log,
